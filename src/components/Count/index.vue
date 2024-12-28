e13ee04abdb4ef7eb910420af3f4977248dce4b6
igitinode <igitinode@gmail.com>
2024-12-28 20:36:08 +0800

    feat(components):vue 的 toRefs 和 pinia 的 storeToRefs 区别

--- src/components/Count/index.vue
+++ src/components/Count/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="count">
-    <h2>当前求和为：{{countStore.sum}}</h2>
+    <h2>当前求和为：{{sum}}</h2>
     <select v-model.number="n">
       <option value="1">1</option>
       <option value="2">2</option>
@@ -13,9 +13,14 @@
 
 <script setup lang="ts" name="Count">
 import { ref } from 'vue';
+import {storeToRefs} from 'pinia';
 import {useCountStore} from '@/store/count';
 
 const countStore = useCountStore()
+// storeToRefs 只关注 state 数据，不会对方法进行 ref 包裹
+// toRefs 关注数据和方法
+const { sum } = storeToRefs(countStore)
+
 let n = ref(1)
 
 function add() {
--- src/components/LoveTalk/index.vue
+++ src/components/LoveTalk/index.vue
@@ -2,15 +2,17 @@
   <div class="talk">
     <button @click="getLoveTalk">获取一句土味情话</button>
     <ul>
-      <li v-for="talk in talkStore.talkList" :key="talk.id">{{ talk.title }}</li>
+      <li v-for="talk in talkList" :key="talk.id">{{ talk.title }}</li>
     </ul>
   </div>
 </template>
 
 <script setup lang="ts" name="LoveTalk">
 import {useTalkStore} from '@/store/loveTalk'
+import { storeToRefs } from 'pinia';
 
 const talkStore = useTalkStore()
+const { talkList } = storeToRefs(talkStore)
 
 function getLoveTalk() {
   talkStore.getATalk()
